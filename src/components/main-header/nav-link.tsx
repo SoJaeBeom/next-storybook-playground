7dff7335eba6817ba72527920755ca721e85ad8c
SoJaeBeom <sojb0826@gmail.com>
2024-05-25 21:15:08 +0900

    추가

---
+++ src/components/main-header/nav-link.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import classes from "./nav-link.module.css";
+
+type Props = {
+  href: string;
+  children: any;
+};
+
+export default function NavLink({ href, children }: Props) {
+  const path = usePathname();
+
+  return (
+    <Link
+      href={href}
+      className={
+        path.startsWith(href)
+          ? `${classes.link} ${classes.active}`
+          : classes.link
+      }
+    >
+      {children}
+    </Link>
+  );
+}
